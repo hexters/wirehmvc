20274645993945da2a3361b42b1c85d5ac743a61
Asep <hexters@gmail.com>
2023-10-16 23:15:29 +0700

    fix path form and attribute

--- src/Console/Commands/LivewireFormCommand.php
+++ src/Console/Commands/LivewireFormCommand.php
@@ -51,6 +51,21 @@ class LivewireFormCommand extends FormCommand
         parent::handle();
     }
 
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        
+        $name = Str::replaceFirst($this->rootNamespace(), '', $name);
+        
+        return base_path("Modules/" . str_replace('\\', '/', $name) . '.php');
+    }
+
     /**
      * Get the root namespace for the class.
      *
