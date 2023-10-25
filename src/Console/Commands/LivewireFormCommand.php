4aeb01d59a1b6b22258dcf219d4eaa0ec93dee2e
Asep <hexters@gmail.com>
2023-10-26 06:03:12 +0700

    Update LivewireFormCommand.php

--- src/Console/Commands/LivewireFormCommand.php
+++ src/Console/Commands/LivewireFormCommand.php
@@ -65,20 +65,7 @@ class LivewireFormCommand extends FormCommand
         
         return base_path("Modules/" . str_replace('\\', '/', $name) . '.php');
     }
-
-    /**
-     * Get the destination class path.
-     *
-     * @param  string  $name
-     * @return string
-     */
-    protected function getPath($name)
-    {
-        $name = Str::replaceFirst($this->rootNamespace(), '', $name);
-        
-        return base_path("Modules/" . str_replace('\\', '/', $name) . '.php');
-    }
-
+    
     /**
      * Get the root namespace for the class.
      *
